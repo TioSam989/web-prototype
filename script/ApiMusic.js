16dea8d894be6af8e26b3a23063ed528821f7fb2
Davi Neves <tio.sam989@hotmail.com>
2022-09-10 19:57:08 +0100

    adding search by track func

--- script/ApiMusic.js
+++ script/ApiMusic.js
@@ -6,36 +6,67 @@ const artistName = document.querySelector("#artistName")
 
 const searchBtn = document.querySelector("#searchBtn")
 
-function getAPIData(artist, music,  limit) {
+function getAPISearchResults(music){
+    const searchURL = `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${music}&api_key=0394adacbab6526b446f377465aae302&format=json&limit=5`
+
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(searchURL, options)
+    .then(res => {
+        res.json()
+            .then( data => {
+
+                
+                let beforeTracks = Object.values(data)[0]
+                let stillBeforeTracks = Object.values(beforeTracks)[4]
+                let track = Object.values(stillBeforeTracks)[0]
+                // console.log(tracks)
+                Object.keys(track).forEach(function (keys) {
+
+
+                    let currentSoung = {
+                        name: track[keys].name,
+                        artist: track[keys].artist
+                    }
+
+                    getAPISimilarData(prepareString(currentSoung.artist),prepareString(currentSoung.name), '10')
+                })
+            })
+    })
+}
+
+function getAPISimilarData(artist, music,  limit) {
     const URL = `https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&track=${music}&artist=${artist}&api_key=${API_key}&limit=${limit}&format=json&autocorrect=1`
     const options = {
         method: 'GET',
         mode: 'cors',
         cache: 'default'
     }
-    console.clear()
+
     console.log(URL)
 
     fetch(URL, options)
         .then(res => { 
             res.json()
-                .then( data => {
+            .then( data => {
+                
                     let beforeTrack = Object.values(data)[0]
                     let track = Object.values(beforeTrack)[0]
+                    
 
-                    
-                    
                     Object.keys(track).forEach(function(keys) {
-                        
-                        console.log(track[keys])
                         
                         let currentSoung = {
                             name: track[keys].name,
-                            artist: track[keys].artist.name
-
+                            artist: track[keys].artist.name,
                         }
-
+                        
                         buildMusicSquare(currentSoung)
+                        console.log(currentSoung)
 
                         // console.log(`Listen to ${currentSoung.name} from ${currentSoung.artist}`)
                     });
@@ -57,10 +88,7 @@ function prepareString(someString) {
 }
 
 searchBtn.addEventListener('click', (e) => {
-    console.log(`artist: ${artistName.value}`)
-    console.log(`music: ${musicName.value}`)
-
-    getAPIData(prepareString(artistName.value), prepareString(musicName.value),  `5`)
+    getAPISearchResults( prepareString(musicName.value))
 
     // e.preventDefault()
 });
