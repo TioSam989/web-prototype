3a5e2b20732e22f99aa89f7105f2bdfd8e760292
Davi Neves <tio.sam989@hotmail.com>
2022-11-10 13:30:34 +0000

    fixing img changer

--- script/script.js
+++ script/script.js
@@ -7,6 +7,7 @@ import { getSptApiRandomResults } from './sptRandom'
 import styled from "daisyui/dist/styled";
 
 const placeImg = document.querySelector('#imgPlace')
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 if (true) {
 
@@ -16,32 +17,39 @@ if (true) {
 async function putWallpaper(where) {
 
 
-try {
-  
-  async function yourFunction() {
-    
-    let randomSong = await getSptApiRandomResults()
-    
-    where.removeAttribute('style')
-    where.setAttribute('style', `background-image: url(${randomSong[0].musicData.album.images[0].url});`)
-    setTimeout(yourFunction, 5000);
+  try {
+
+    async function yourFunction() {
+
+      let randomSong = await getSptApiRandomResults()
+      for (let index = 0; index < randomSong.length; index++) {
+
+        where.removeAttribute('style')
+        where.setAttribute('style', `background-image: url(${randomSong[index].musicData.album.images[0].url});`)
+        await delay(5000)
+
+      }
+
+      
+      console.log(randomSong)
+      setTimeout(yourFunction, 0);
+    }
+
+    yourFunction();
+
+  } catch (error) {
+    console.error(error)
+    async function yourFunction() {
+
+      let randomSong = await getSptApiRandomResults()
+
+      where.removeAttribute('style')
+      where.setAttribute('style', `background-image: url(${randomSong[0].musicData.album.images[0].url});`)
+      setTimeout(yourFunction, 5000);
+    }
+
+    yourFunction();
   }
-  
-  yourFunction();
-  
-} catch (error) {
-  console.error(error)
-  async function yourFunction() {
-    
-    let randomSong = await getSptApiRandomResults()
-    
-    where.removeAttribute('style')
-    where.setAttribute('style', `background-image: url(${randomSong[0].musicData.album.images[0].url});`)
-    setTimeout(yourFunction, 5000);
-  }
-  
-  yourFunction();
-}
 
 
 
